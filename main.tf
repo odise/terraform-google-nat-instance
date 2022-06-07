7175a677110cc1c73b8462c477dc53dd60f77723
Jan Nabbefeld <jan@odise.de>
2022-06-07 16:11:43 +0200

    polish it up

--- main.tf
+++ main.tf
@@ -1,60 +1,65 @@
-resource random_id instance_suffix {
+resource "random_id" "instance_suffix" {
   byte_length = 2
   keepers = {
-    zone = var.zone
-    address = var.address
-    startup_script = local.startup_script
+    zone                 = var.zone
+    address              = var.address
+    startup_script       = local.startup_script
     startup_script_parts = sha256(jsonencode(local.instance_metadata))
-    machine_type = var.machine_type
-    disk_size = var.disk_size
-    disk_type = var.disk_type
+    machine_type         = var.machine_type
+    disk_size            = var.disk_size
+    disk_type            = var.disk_type
   }
 }
 
-resource random_id address_suffix {
+resource "random_id" "address_suffix" {
   byte_length = 2
   keepers = {
     region = local.region
   }
 }
 
-resource google_compute_address address {
-  name = "nat-instance-${local.region}-${random_id.address_suffix.hex}"
+resource "google_compute_address" "address" {
+  project      = var.project
+  name         = "nat-instance-${local.region}-${random_id.address_suffix.hex}"
   address_type = "INTERNAL"
-  region = random_id.address_suffix.keepers.region
+  region       = random_id.address_suffix.keepers.region
+  subnetwork   = var.subnetwork
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource google_compute_instance instance {
-  name = local.instance_name
-  zone = random_id.instance_suffix.keepers.zone
-  can_ip_forward = true
-  machine_type = random_id.instance_suffix.keepers.machine_type
+resource "google_compute_instance" "instance" {
+  project = var.project
+
+  name                    = local.instance_name
+  zone                    = random_id.instance_suffix.keepers.zone
+  can_ip_forward          = true
+  machine_type            = random_id.instance_suffix.keepers.machine_type
   metadata_startup_script = random_id.instance_suffix.keepers.startup_script
-  metadata = local.instance_metadata
-  tags = [local.instance_name]
+  metadata                = local.instance_metadata
+  tags                    = [local.instance_name]
 
   boot_disk {
     initialize_params {
-      size = random_id.instance_suffix.keepers.disk_size
-      type = random_id.instance_suffix.keepers.disk_type
+      size  = random_id.instance_suffix.keepers.disk_size
+      type  = random_id.instance_suffix.keepers.disk_type
       image = "debian-cloud/debian-10"
     }
   }
 
   network_interface {
-    network = "default"
+    network    = var.network
     network_ip = google_compute_address.address.address
+    subnetwork = var.subnetwork
     access_config {
       nat_ip = random_id.instance_suffix.keepers.address
     }
   }
 }
 
-resource null_resource delay_between_instance_and_route {
+resource "null_resource" "delay_between_instance_and_route" {
   provisioner "local-exec" {
     command = "sleep ${var.wait_duration}"
   }
@@ -64,13 +69,14 @@ resource null_resource delay_between_instance_and_route {
   }
 }
 
-resource google_compute_route route {
-  name = google_compute_instance.instance.name
-  network = "default"
-  dest_range = "0.0.0.0/0"
-  tags = local.network_tags
-  priority = var.route_priority
-  next_hop_instance = google_compute_instance.instance.self_link
+resource "google_compute_route" "route" {
+  name                   = google_compute_instance.instance.name
+  project                = var.project
+  network                = "default"
+  dest_range             = "0.0.0.0/0"
+  tags                   = local.network_tags
+  priority               = var.route_priority
+  next_hop_instance      = google_compute_instance.instance.self_link
   next_hop_instance_zone = google_compute_instance.instance.zone
-  depends_on = [null_resource.delay_between_instance_and_route]
+  depends_on             = [null_resource.delay_between_instance_and_route]
 }
